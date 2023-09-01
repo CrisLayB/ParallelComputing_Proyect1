acce73fd88d8c642e73994d4a1bd4747797e4b4f
Cristian Laynez <54615310+CrisLayB@users.noreply.github.com>
2023-09-01 19:24:39 -0300

    More details

--- circle.cpp
+++ circle.cpp
@@ -17,6 +17,7 @@ private:
     double z;
     int directionX;
     int directionY;
+    double speed;
 
 public:
     Circle(double r, GLfloat* c, double x, double y, double z):
@@ -24,6 +25,7 @@ public:
     {
         directionY = randomOne();
         directionX = randomOne();
+        speed = 0.02;
     }
 
     void update()
@@ -40,30 +42,37 @@ public:
 private:
     void bounce()
     {
-        x += directionX * 0.02;
+        x += directionX * speed;
         if(x > 1)
         {
             directionX = -1;
             directionY = randomOne();
+            speed = floatRandom(0.01, 0.04);
+            changueColor();
         }
         else if(x < -1)
         {
             directionX = 1;
             directionY = randomOne();
+            speed = floatRandom(0.01, 0.04);
+            changueColor();
         }
 
-        y += directionY * 0.02;
+        y += directionY * speed;
         if (y > 1)
         {
             directionY = -1;
             directionX = randomOne();
+            speed = floatRandom(0.01, 0.04);
+            changueColor();
         } 
         else if (y < -1)
         {
             directionY = 1;
             directionX = randomOne();
+            speed = floatRandom(0.01, 0.04);
+            changueColor();
         }
-
     }
 
     int randomOne()
@@ -73,5 +82,27 @@ private:
 
         std::uniform_int_distribution<> dist(0, 1);
         return dist(gen) * 2 - 1;
+    }
+
+    float floatRandom(float min, float max)
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> dis(min, max);
+
+        return dis(gen);
+    }
+
+    void changueColor()
+    {
+        GLfloat colorRandom[] = {
+            floatRandom(0.0f, 1.0f), 
+            floatRandom(0.0f, 1.0f), 
+            floatRandom(0.0f, 1.0f)
+        };
+
+        for (int i = 0; i < 3; i++) {
+            color[i] = colorRandom[i];
+        }
     }
 };
